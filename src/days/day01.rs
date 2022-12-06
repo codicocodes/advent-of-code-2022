019025d1831596b3fe2d633ce9483315b5f2c811
codico <codicocodes@gmail.com>
2022-12-06 20:26:24 +0100

    Refactor day1 a

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -1,17 +1,30 @@
 use std::cmp::max;
 
-pub fn solve(input: &str) -> i32 {
+fn parse_calories_by_elf(input: &str) -> Vec<i32> {
     let split_inputs = str::trim(input).split("\n\n");
-    let mut max_calories = 0;
+    let mut calories: Vec<i32> = vec![];
     for elf in split_inputs {
         let mut elf_calories = 0;
         for food in elf.split("\n") {
             let food_calories = food.parse::<i32>().unwrap();
             elf_calories += food_calories;
         }
+        calories.push(elf_calories)
+    }
+    return calories;
+}
+
+fn calculate_max_calories(calories: Vec<i32>) -> i32 {
+    let mut max_calories = 0;
+    for elf_calories in calories {
         max_calories = max(max_calories, elf_calories);
     }
-    return max_calories;
+    return max_calories
+}
+
+pub fn solve(input: &str) -> i32 {
+    let calories = parse_calories_by_elf(input);
+    return calculate_max_calories(calories)
 }
 
 #[cfg(test)]
@@ -35,6 +48,7 @@ mod tests {
 
 10000";
 
-        assert_eq!(solve(input), 24000);
+        let calories = parse_calories_by_elf(input);
+        assert_eq!(calculate_max_calories(calories), 24000);
     }
 }
