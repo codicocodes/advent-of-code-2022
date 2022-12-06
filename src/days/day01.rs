4b9ae334893da6d46b9e38ba26079a847f8b0113
codico <codicocodes@gmail.com>
2022-12-06 21:21:47 +0100

    Solve day1 b

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -22,18 +22,39 @@ fn calculate_max_calories(calories: Vec<i32>) -> i32 {
     return max_calories
 }
 
-pub fn solve(input: &str) -> i32 {
+fn calculate_top3_calories(mut calories: Vec<i32>) -> i32 { calories.sort();
+    calories.reverse();
+    let mut aggregated_calories = 0;
+    for idx in 0..3 {
+        let elf_calories = calories[idx];
+        aggregated_calories += elf_calories;
+    }
+    return aggregated_calories
+}
+
+pub fn solve_a(input: &str) {
     let calories = parse_calories_by_elf(input);
-    return calculate_max_calories(calories)
+    let max_calories = calculate_max_calories(calories);
+    println!("{max_calories}")
+}
+
+
+pub fn solve_b(input: &str) {
+    let calories = parse_calories_by_elf(input);
+    let top3_calories = calculate_top3_calories(calories);
+    println!("{top3_calories}")
+}
+
+pub fn solve(input: &str) {
+    solve_a(input);
+    solve_b(input);
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_max_calories() {
-        let input = "1000
+    const EXAMPLE_INPUT: &str = "1000
 2000
 3000
 
@@ -48,7 +69,15 @@ mod tests {
 
 10000";
 
-        let calories = parse_calories_by_elf(input);
+    #[test]
+    fn test_max_calories() {
+        let calories = parse_calories_by_elf(EXAMPLE_INPUT);
         assert_eq!(calculate_max_calories(calories), 24000);
+    }
+
+    #[test]
+    fn test_top3_calories() {
+        let calories = parse_calories_by_elf(EXAMPLE_INPUT);
+        assert_eq!(calculate_top3_calories(calories), 45000);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,5 @@ mod days;
 fn main() {
     let file_path = "src/inputs/day01.txt";
     let input = std::fs::read_to_string(file_path).unwrap();
-    let result = days::day01::solve(&input);
-    println!("{result}");
+    days::day01::solve(&input);
 }
